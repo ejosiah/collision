93927cb756013503c9f23fd363dda35ecf804ddb
Josiah Ebhomenye <josiah.ebhomenye@news.co.uk>
2017-07-08 13:27:55 +0100

    some updates

--- collision/CollisionTestScene.h
+++ collision/CollisionTestScene.h
@@ -9,10 +9,49 @@ using namespace std;
 using namespace glm;
 using namespace ncl::gl;
 
+class CollisionTestScene;
+
+//class Mode {
+//public:
+//	virtual void init() = 0;
+//	virtual string msg() = 0;
+//	virtual void processInput() = 0;
+//protected:
+//	CollisionTestScene* scene;
+//};
+
+class SelectedObjectController : public _3DMotionEventHandler {
+public:
+	virtual void onMotion(const _3DMotionEvent& e) {
+		if (_object) {
+			_object->move(e.translation);
+			_object->rotate(e.rotation);
+		}
+	};
+	virtual void onNoMotion() {
+	
+	}
+
+	void object(Object* obj) {
+		_object = obj;
+	}
+
+	void clear() {
+		_object = nullptr;
+	}
+
+private:
+	Object* _object;
+};
+
+
+enum Mode { CREATE, CAMERA, SELECT };
+
 class CollisionTestScene : public Scene {
 public:
 	CollisionTestScene() : Scene("Collision Test") {
-
+		currentMode = CREATE;
+		_motionEventHandler = &objectController;
 	}
 
 	virtual void init() override {
@@ -24,11 +63,7 @@ public:
 		auto teapot = new TeaPotObject;
 		//object->setShader(_shader);
 		teapot->setShader(_shader);
-		objects.push_back(teapot);
-
-		addKeyListener([&](const Key& key) {
-			cameraController.processUserInput();
-		});
+	//	objects.push_back(teapot);
 
 		lightModel.colorMaterial = true;
 	}
@@ -55,12 +90,45 @@ public:
 
 	void displayText() {
 		stringstream ss;
-		ss << "FPS: " << fps;
+		ss << "FPS: " << fps << endl;
+		
+		switch (currentMode) {
+		case CREATE:
+			ss << "Create Mode:" << endl;
+			ss << "\t1 : Sphere";
+			ss << "\t2 : Cube";
+			ss << "\t3 : Cyninder";
+			ss << "\t4 : Cone";
+			ss << "\t5 : Teapot";
+			break;
+		}
 		font->render(ss.str(), 10, _height - 20);
+	}
+
+	virtual void processInput(Keyboard& keyboard) override {
+		Object* object = nullptr;
+		switch (currentMode) {
+		case CREATE:
+			if (keyboard.NUM_1.pressed()) {
+				object = new SphereObject;
+			}
+			else if (keyboard.NUM_5.pressed()) {
+				object = new TeaPotObject;
+			}
+			break;
+		}
+		if (object) {
+			object->init();
+			object->setShader(_shader);
+			objects.push_back(object);
+		}
 	}
 
 private:
 	vector<Object*> objects;
+	Object* selectedObj;
 	CameraController cameraController;
 	Font* font;
+	Mode currentMode;
+	SelectedObjectController objectController;
 };
--- collision/Object.h
+++ collision/Object.h
@@ -27,11 +27,15 @@ public:
 		draw(*shader, camera);
 	}
 
-	virtual void move(vec3& amount) {
+	virtual void move(const vec3& amount) {
 		position += amount;
 	}
 
-	virtual void rotate(float yaw, float pitch, float roll){
+	virtual void rotate(const vec3& amount) {
+		rotate(amount.x, amount.y, amount.z);
+	}
+
+	virtual void rotate(float pitch, float yaw, float roll){
 		orientation *= Orientation({ radians(pitch), radians(yaw), radians(roll) });
 	}
 
